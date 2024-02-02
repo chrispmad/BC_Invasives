b60ea4bde6669af96ed7951e24de7cfcfbddae6a
Madsen <Chris.Madsen@gov.bc.ca>
2024-02-02 14:55:56 -0800

    updated

--- publish_app.R
+++ publish_app.R
@@ -24,17 +24,78 @@ if(!file.exists(paste0('publishing_results/publishing_results_',Sys.Date(),'_err
       to = 'app/www/Master Incidence Report Records.xlsx'
     ),
     error = function(e) {
-      publishing_results$error_at = 'excel_copying'
+      publishing_results$error_at = 'excel_copying_master_incident'
       publishing_results$error = TRUE
     }
   )
-  print('Copied new version into app folder.')
+  print('Copied new version of master incident tracking sheet into app folder.')
 
-  # Did the file copy work?
-  if(!file.exists('app/www/Master Incidence Report Records.xlsx')){
+  # Update the priority invasive species excel sheet and pull out species
+  tryCatch(
+    file.copy(
+      from = "J:/2 SCIENCE - Invasives/GENERAL/Inter-governmental-relations/IMISWG/Priority species list/Provincial Priority IS List_2023-11-15.xlsx",
+      to = 'app/www/AIS_priority_species.xlsx'
+    ),
+    error = function(e) {
+      publishing_results$error_at = 'excel_copying_priority_list'
+      publishing_results$error = TRUE
+    }
+  )
+  print('Copied new version of priority list into app folder.')
+
+  # Did the file copying work?
+  if(!file.exists('app/www/Master Incidence Report Records.xlsx') | !file.exists('app/www/AIS_priority_species.xlsx')){
     publishing_results$error_at = 'excel_copying'
     publishing_results$error = TRUE
   }
+
+  # Read in priority list of AIS species (excel file), use it to update selectInput
+  pr_sp = readxl::read_excel('app/www/AIS_priority_species.xlsx')
+
+  # Format this terrible excel sheet
+  data_start = min(which(pr_sp[,1] == 'Disease'))
+
+  pr_sp = pr_sp[data_start:nrow(pr_sp),]
+
+  names(pr_sp) <- c("group","status","name","genus","species")
+
+  # Just for our species of interest.
+  pr_sp = pr_sp |>
+    dplyr::filter(group == 'Fish' | name %in% c("Whirling disease") | stringr::str_detect(name, '(mussel|crayfish)'))
+
+  # Split out grouped species names into separate rows.
+  pr_sp = pr_sp |>
+    dplyr::mutate(name = dplyr::case_when(
+      stringr::str_detect(name, 'Snakehead') ~ 'Northern Snakehead',
+      name == 'Bullhead' ~ 'Yellow Bullhead',
+      name == 'Red bellied piranha*' ~ 'Red bellied piranha',
+      stringr::str_detect(name, '\\(') ~ stringr::str_remove_all(name,'\\(.*'),
+      T ~ name
+    )) |>
+    dplyr::mutate(name = stringr::str_squish(name)) |>
+    dplyr::bind_rows(
+      tidyr::tibble(
+        group = rep("Fish", 4),
+        status = c("prevent","prevent",rep("management",2)),
+        name = c("Blotched Snakehead","Rainbow Snakehead",'Black Bullhead','Brown Bullhead'),
+        genus = c("Channa",'Channa','Ameiurus','Ameiurus'),
+        species = c("maculata","bleheri",'melas','nebulosus')
+      )
+    ) |>
+    dplyr::filter(name != 'Bullhead') |>
+    dplyr::arrange(name)
+
+  write.csv(pr_sp, 'app/www/priority_species_table2.csv', row.names = F)
+
+  # Do record search for all species of interest! This takes a minute.
+  occ_dat_search_results = pr_sp$name |>
+    lapply(\(x) tryCatch(bcinvadeR::grab_aq_occ_data(x),error=function(e)return(NULL)))
+
+  occ_dat_res_b = dplyr::bind_rows(occ_dat_search_results)
+
+  sf::write_sf(occ_dat_res_b, 'app/www/occ_dat.gpkg')
+
+  print("Written SF object of occurrence data records to app's www folder")
 
   if(!publishing_results$error){
     # Update bcinvadeR R package
